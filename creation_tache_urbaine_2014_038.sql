b5a89dc3a246a95dc261057915221cfe47391070
cbadol <christophe.badol@isere.gouv.fr>
2015-02-25 18:53:47 +0100

    Quatrième version du code SQL
    Optimisation du code

--- creation_tache_urbaine_2014_038.sql
+++ creation_tache_urbaine_2014_038.sql
@@ -1,103 +1,58 @@
-﻿--DROP TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038; --à utiliser pour débugage rapide
---DROP TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038; --à utiliser pour débugage rapide
+﻿-------------------------------------------------------------------------------------------------------------
+-- Partie 1 : Génération du bâti agrégé pour d'autres applications (Application de la loi montagne notamment)
+-------------------------------------------------------------------------------------------------------------
+-- Debug : DROP TABLE l_bati_agrege_2014_038;
 
-------------------------------------------------
--- Partie Création de la table du bâti agrégé --
-------------------------------------------------
-CREATE TABLE tampon_bati_2014_plus1 AS SELECT ST_Buffer(the_geom,0,01) AS the_geom FROM cadastre__dgi.n_bati_dgi_038_2014; -- WHERE codcomm='38001'; --à utiliser pour débugage rapide -- Buffer de 1 cm sur chaque objet de n_bati_dgi_038_2014
-
-CREATE TABLE fusion_tampon_bati_2014_plus1 AS SELECT ST_Union(the_geom) AS the_geom FROM tampon_bati_2014_plus1; -- Fusion des tampons en un seul objet
-
-CREATE TABLE fusion_tampon_bati_2014 AS SELECT ST_Buffer(the_geom,-0,01) AS the_geom FROM fusion_tampon_bati_2014_plus1; -- Buffer de – 1 cm sur chaque objet de  fusion_tampon_bati_2014_plus1
-
---- Création de la table du bâti agrégé
-CREATE TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038
+CREATE TABLE l_bati_agrege_2014_038
 (
+  nbatidur integer,
+  nbatileg integer,
   the_geom geometry(Polygon,2154)
+)
+WITH (
+  OIDS=TRUE
 );
-INSERT INTO foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 SELECT (ST_Dump(the_geom)).geom as the_geom from fusion_tampon_bati_2014; --- Désagrégation des tampons fusionnés.
+ALTER TABLE l_bati_agrege_2014_038
+  OWNER TO postgres;
 
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 -- ajout de deux colonnes supplémentaires pour renseigner le nombre de batiments qui ont été agrégés
-	ADD COLUMN nbatidur integer,
-	ADD COLUMN nbatileg integer;
+-- Buffer +0,01, ST_Union, Buffer -0,01, ST_Dump
+INSERT INTO l_bati_agrege_2014_038 (the_geom) SELECT (ST_Dump(ST_Buffer(ST_Union(ST_Buffer(the_geom,0,01)),-0,01))).geom as the_geom from cadastre_dgi.n_bati_dgi_038_2014; --debug : WHERE cadastre_dgi.n_bati_dgi_038_2014.codcomm = '38001';
 
-UPDATE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 SET nbatidur = (SELECT count(*) FROM cadastre__dgi.n_bati_dgi_038_2014 WHERE (dur='Bâti dur' or dur='Bati dur' or dur='01') and ST_Intersects(cadastre__dgi.n_bati_dgi_038_2014.the_geom, foncier_sol__n_occupation_sol.l_bati_agrege_2014_038.the_geom)); -- Compte le nombre de batiments en dur qui ont permis de générer ce batiment agrégé et met la valeur dans le champs  nbatidur
-UPDATE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 SET nbatileg = (SELECT count(*) FROM cadastre__dgi.n_bati_dgi_038_2014 WHERE (dur='Bâti léger' or dur='Bati leger' or dur='02') and ST_Intersects(cadastre__dgi.n_bati_dgi_038_2014.the_geom, foncier_sol__n_occupation_sol.l_bati_agrege_2014_038.the_geom)); -- Compte le nombre de batiments legers qui ont permis de générer ce batiment agrégé et met la valeur dans le champs nbatileg
+-- Mise à jour des données attributaires
+UPDATE l_bati_agrege_2014_038 SET nbatidur = (SELECT count(*) FROM cadastre_dgi.n_bati_dgi_038_2014 WHERE dur='Bâti dur' and ST_Intersects(cadastre_dgi.n_bati_dgi_038_2014.the_geom, l_bati_agrege_2014_038.the_geom)); -- Compte le nombre de batiments en dur qui ont permis de générer ce batiment agrégé et met la valeur dans le champs  nbatidur
+UPDATE l_bati_agrege_2014_038 SET nbatileg = (SELECT count(*) FROM cadastre_dgi.n_bati_dgi_038_2014 WHERE dur='Bâti léger' and ST_Intersects(cadastre_dgi.n_bati_dgi_038_2014.the_geom, l_bati_agrege_2014_038.the_geom)); -- Compte le nombre de batiments legers qui ont permis de générer ce batiment agrégé et met la valeur dans le champs nbatileg
 
--- Pour déplacer le champs géométrique à la fin
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 ADD COLUMN temp_geom geometry(Polygon,2154);
-UPDATE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 SET temp_geom = st_buffer(the_geom, 0.0);   
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 DROP COLUMN the_geom;
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 RENAME COLUMN temp_geom TO the_geom;
+--Index géométrique
+CREATE INDEX l_bati_agrege_2014_038_the_geom_gist ON l_bati_agrege_2014_038 USING gist (the_geom);
 
--- Index et contraintes sur la base :
-CREATE INDEX l_bati_agrege_2014_038_the_geom_gist ON foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 USING gist (the_geom); -- Index créé.
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 ADD CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POLYGON'::text OR the_geom IS NULL); --création à nouveau de la contrainte POLYGON
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 ADD CONSTRAINT geometry_valid_check CHECK (ST_IsValid(the_geom));
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 ADD CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 2154)
+--------------------------------------------
+-- Partie 2 : Génération de la tache urbaine
+--------------------------------------------
+-- Debug : DROP TABLE l_tache_urbaine_2014_038;
 
--- Droits sur la base :
-ALTER TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038
-  OWNER TO gb_adm;
-GRANT ALL ON TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 TO gb_adm;
-GRANT ALL ON TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 TO geobase38_administrateurs WITH GRANT OPTION;
-GRANT SELECT ON TABLE foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 TO geobase38_consultation;
-
------------------------------------------------------
--- Partie Création de la table de la tache urbaine --
------------------------------------------------------
-CREATE TABLE tu_2014_plus50_038 AS SELECT ST_Buffer(the_geom,50) AS the_geom FROM foncier_sol__n_occupation_sol.l_bati_agrege_2014_038; -- Buffer de 50m sur chaque objet du foncier_sol__n_occupation_sol.l_bati_agrege_2014_038
-
-CREATE TABLE tu_2014_plus50_union_038 AS SELECT ST_Union(the_geom) AS the_geom FROM tu_2014_plus50_038; -- fusion des tampons en un seul objet
-
-CREATE TABLE tu_2014_moins40_038 AS SELECT ST_Buffer(the_geom,-40) AS the_geom FROM tu_2014_plus50_union_038; -- Buffer de -40m sur chaque objet de tu_2014_plus50_union_038
-
-CREATE TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038
+CREATE TABLE l_tache_urbaine_2014_038
 (
+  nbatidur integer,
+  nbatileg integer,
+  nbagrege integer,
   the_geom geometry(Polygon,2154)
+)
+WITH (
+  OIDS=TRUE
 );
-INSERT INTO foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 SELECT (ST_Dump(the_geom)).geom as the_geom from tu_2014_moins40_038; -- Désagrégation des tampons fusionnés.
+ALTER TABLE l_tache_urbaine_2014_038
+  OWNER TO postgres;
 
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 -- ajout de 3 colonnes supplémentaires pour renseigner le nombre de batiments qui ont été agrégés
-	ADD COLUMN nbatidur integer,
-	ADD COLUMN nbatileg integer,
-	ADD COLUMN nbagrege integer;
+  -- Buffer +50, ST_Union, Buffer -40, ST_Dump
+INSERT INTO l_tache_urbaine_2014_038 (the_geom) SELECT (ST_Dump(ST_Buffer(ST_Union(ST_Buffer(the_geom,50)),-40))).geom as the_geom from cadastre_dgi.n_bati_dgi_038_2014; --debug : WHERE cadastre_dgi.n_bati_dgi_038_2014.codcomm = '38001';
 
-UPDATE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 SET nbatidur = (SELECT count(*) FROM cadastre__dgi.n_bati_dgi_038_2014 WHERE (dur='Bâti dur' or dur='Bati dur' or dur='01') and ST_Intersects(cadastre__dgi.n_bati_dgi_038_2014.the_geom, foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments en dur dans la tache urbaine et met la valeur dans le champs  nbatidur
-UPDATE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 SET nbatileg = (SELECT count(*) FROM cadastre__dgi.n_bati_dgi_038_2014 WHERE (dur='Bâti léger' or dur='Bati leger' or dur='02') and ST_Intersects(cadastre__dgi.n_bati_dgi_038_2014.the_geom, foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments en dur dans la tache urbaine et met la valeur dans le champs nbatileg
-UPDATE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 SET nbagrege = (SELECT count(*) FROM foncier_sol__n_occupation_sol.l_bati_agrege_2014_038 WHERE ST_Intersects(foncier_sol__n_occupation_sol.l_bati_agrege_2014_038.the_geom, foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments de la couche des batiments agrégés compris dans la tache urbaine et met la valeur dans le champs  nbagrege
+-- Mise à jour des données attributaires
+UPDATE l_tache_urbaine_2014_038 SET nbatidur = (SELECT count(*) FROM cadastre_dgi.n_bati_dgi_038_2014 WHERE dur='Bâti dur' and ST_Intersects(cadastre_dgi.n_bati_dgi_038_2014.the_geom, l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments en dur dans la tache urbaine et met la valeur dans le champs  nbatidur
+UPDATE l_tache_urbaine_2014_038 SET nbatileg = (SELECT count(*) FROM cadastre_dgi.n_bati_dgi_038_2014 WHERE dur='Bâti léger' and ST_Intersects(cadastre_dgi.n_bati_dgi_038_2014.the_geom, l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments en dur dans la tache urbaine et met la valeur dans le champs nbatileg
+UPDATE l_tache_urbaine_2014_038 SET nbagrege = (SELECT count(*) FROM l_bati_agrege_2014_038 WHERE ST_Intersects(l_bati_agrege_2014_038.the_geom, l_tache_urbaine_2014_038.the_geom)); -- Compte le nombre de batiments de la couche des batiments agrégés compris dans la tache urbaine et met la valeur dans le champs  nbagrege
 
--- Pour déplacer le champs géométrique à la fin
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 ADD COLUMN temp_geom geometry(Polygon,2154);
-UPDATE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 SET temp_geom = st_buffer(the_geom, 0.0);
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 DROP COLUMN the_geom;
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 RENAME COLUMN temp_geom TO the_geom;
+--Index géométrique
+CREATE INDEX l_tache_urbaine_2014_038_the_geom_gist ON l_tache_urbaine_2014_038 USING gist (the_geom);
 
--- Index et contraintes sur la base :
-CREATE INDEX l_tache_urbaine_2014_038_the_geom_gist ON foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 USING gist (the_geom);
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 ADD CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POLYGON'::text OR the_geom IS NULL); --création à nouveau de la contrainte POLYGON
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 ADD CONSTRAINT geometry_valid_check CHECK (ST_IsValid(the_geom));
--- contrainte de projection en 2154 ?
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 ADD CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 2154)
-
--- Droits sur la base :
-ALTER TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038
-  OWNER TO gb_adm;
-GRANT ALL ON TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 TO gb_adm;
-GRANT ALL ON TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 TO geobase38_administrateurs WITH GRANT OPTION;
-GRANT SELECT ON TABLE foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038 TO geobase38_consultation;
-
-----------------------------------------------------
--- Suppression des tables provisoires necessaires --
-----------------------------------------------------
-DROP TABLE tampon_bati_2014_plus1;
-DROP TABLE fusion_tampon_bati_2014_plus1;
-DROP TABLE fusion_tampon_bati_2014;
-DROP TABLE tu_2014_plus50_038;
-DROP TABLE tu_2014_plus50_union_038;
-DROP TABLE tu_2014_moins40_038;
-
--------------------------
--- Requête de synthèse --
--------------------------
-SELECT count(*) AS nb_objets, sum(ST_Area(the_geom)) AS surface FROM foncier_sol__n_occupation_sol.l_tache_urbaine_2014_038;
+-- Rq de Synthèse
+SELECT count(*) AS nb_objets, sum(ST_Area(the_geom)) AS surface FROM l_tache_urbaine_2014_038;
